7aeebf51204e950c3eb741dca8a4e55ddb06e1bd
YukieChang <changyyq@gmail.com>
2019-09-27 04:36:39 -0400

    link collector updated

--- app/src/main/java/com/newApp/numad19f_yangyuqichang/LinkCollector.java
+++ app/src/main/java/com/newApp/numad19f_yangyuqichang/LinkCollector.java
@@ -3,6 +3,8 @@ package com.newApp.numad19f_yangyuqichang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.util.Linkify;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -23,6 +25,29 @@ public class LinkCollector extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_link_collector);
+        text = (EditText)findViewById(R.id.txtInput);
+        List = (ListView)findViewById(R.id.txtList);
+        Insert = (Button)findViewById(R.id.btnInsert);
+        Insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getInput = text.getText().toString();
+
+                 if (getInput == null || getInput.trim().equals("")){
+                    Toast.makeText(getBaseContext(), "The link is empty.", Toast.LENGTH_LONG).show();
+                }
+
+                else{
+                     Toast.makeText(getBaseContext(), "The link has been added to the list.", Toast.LENGTH_LONG).show();
+                     addArray.add(getInput);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(LinkCollector.this,android.R.layout.simple_list_item_1, addArray);
+                    List.setAdapter(adapter);
+                    ((EditText) findViewById(R.id.txtInput)).setText(" ");
+
+                 }
+            }
+        });
 
     }
 }
+
